df0b3c5bf9e40effbad528fb553bc6523174bc97
Carlos <97473672+Cralon654@users.noreply.github.com>
2023-08-26 13:26:16 +0200

    Primeros pasos

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,1 +1,50 @@
-// Write all the code here
+import React, { useState } from "react";
+import {
+    MDBContainer,
+    MDBTabs,
+    MDBTabsItem,
+    MDBTabsLink,
+    MDBTabsContent,
+    MDBTabsPane,
+    MDBBtn,
+    MDBIcon,
+    MDBInput,
+    MDBCheckbox,
+} from "mdb-react-ui-kit";
+
+function Login() {
+    const [justifyActive, setJustifyActive] = useState("tab1");
+    const handleJustifyClick = (value) => {
+        if (value === justifyActive) {
+            return;
+        }
+        setJustifyActive(value);
+    };
+
+    return (
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+            <MDBTabs pills justify className="mb-3 d-flex flex-row justify-content-between">
+                <MDBTabsItem>
+                    <MDBTabsLink onClick={() => handleJustifyClick("tab1")} active={justifyActive === "tab1"}>
+                        Login
+                    </MDBTabsLink>
+                </MDBTabsItem>
+                <MDBTabsItem>
+                    <MDBTabsLink onClick={() => handleJustifyClick("tab2")} active={justifyActive === "tab2"}>
+                        Regístrate
+                    </MDBTabsLink>
+                </MDBTabsItem>
+            </MDBTabs>
+            <MDBTabsContent>
+                <MDBTabsPane show={justifyActive === "tab1"}>
+                    {/* ...contenido de tab1... */}
+                </MDBTabsPane>
+                <MDBTabsPane show={justifyActive === "tab2"}>
+                    {/* ...contenido de tab2... */}
+                </MDBTabsPane>
+            </MDBTabsContent>
+        </MDBContainer>
+    );
+}
+
+export default Login;
--- src/components/myOrders/MyOrders.jsx
+++ src/components/myOrders/MyOrders.jsx
@@ -22,7 +22,22 @@ const MyOrders = () => {
 
           <tbody>
 //     Add the code for the table body
-           
+            <tbody>
+              {arr.map((i) => (
+                <tr key={i}>
+                  <td>#ON????????</td>
+                  <td>En curso</td>
+                  <td>3</td>
+                  <td>€ {16.00}</td>
+                  <td>pago contra entrega</td>
+                  <td>
+                    <Link to={`/order/${"ON00012345"}`}>
+                      <AiOutlineEye />
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>;
           </tbody>
         </table>
       </main>
